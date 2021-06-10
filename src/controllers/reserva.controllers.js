dcefa03c002e5afd0172ecea9c55841a7b8827d9
acem0301 <acem0301@gmail.com>
2021-06-09 22:19:32 -0400

    Se finaliza método de control de reserva.

--- src/controllers/reserva.controllers.js
+++ src/controllers/reserva.controllers.js
@@ -1,29 +1,25 @@
 /* eslint-disable no-await-in-loop */
+const moment = require('moment');
 const { reserva, restaurante } = require('../models');
 
 const controlReservation = async (restaurante_id, mesa_id, fecha_desde, fecha_hasta) => {
   try {
-    const getReservation = await reserva.findAll({
-      where: { restaurante_id, mesa_id },
+    const restaurant = await restaurante.findOne({
+      where: { id: restaurante_id },
     });
 
-    getReservation.forEach((r) => {
-      if (
-        (fecha_desde >= r.fecha_desde && fecha_desde < r.fecha_hasta) ||
-        (fecha_hasta > r.fecha_desde && fecha_hasta <= r.fecha_hasta)
-      ) {
-        /*Error */
-      } else {
-        /*Control cant_horas */
-        const restaurant = restaurante.findOne({
-          where: { restaurante_id },
-        });
+    const galleta = moment(fecha_hasta);
+    const coquito = moment(fecha_desde);
 
-        const getMaxHours = restaurant;
-      }
-    });
+    const horasDeseadas = galleta.diff(coquito) / (1000 * 60 * 60);
 
-    await Promise.all(promises);
+    const sobrepasaHoraMaxima = horasDeseadas > restaurant.horas_maxima_por_mesa;
+
+    if (sobrepasaHoraMaxima) {
+      return true;
+    }
+
+    return false;
   } catch (error) {
     throw new Error(error);
   }
@@ -33,7 +29,19 @@ const saveReservation = async (req, res, next) => {
   try {
     const { restaurante_id, mesa_id, usuario_id, fecha_desde, fecha_hasta } = req.body;
 
-    await controlReservation(restaurante_id, mesa_id, fecha_desde, fecha_hasta);
+    const sobrepasaHoraMaxima = await controlReservation(
+      restaurante_id,
+      mesa_id,
+      fecha_desde,
+      fecha_hasta
+    );
+
+    if (sobrepasaHoraMaxima) {
+      return res.status(200).json({
+        success: true,
+        message: 'El restaurante no permite esa cantidad de reserva por mesa',
+      });
+    }
 
     const reservation = await reserva.create({
       restaurante_id,
